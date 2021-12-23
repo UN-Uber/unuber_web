a9397f3c1a524ee2769b8332e3a7e2b578489691
OscarAlejandroGomez <ogomezs@unal.edu.co>
2021-12-23 17:15:13 -0500

    Agregación de los reducers y redux thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
-import { createStore} from 'redux';
+import { createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from './reducers/index';
+import thunk from 'redux-thunk';
+
+const initialState = {};
+const middleware = [thunk];
 
 const store  = createStore(
     reducers,
-    {}
-)
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
