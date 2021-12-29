b70b3541307e7f7813111b607ca34803e23ecc64
Juan Camilo Villota Ballesteros <38733879+jcvillotab@users.noreply.github.com>
2021-12-29 17:31:23 -0500

    First Version of login

--- src/graphql/server.ts
+++ src/graphql/server.ts
@@ -3,15 +3,12 @@ import {
     InMemoryCache,
   } from "@apollo/client";
 
-export const clientAuth = new ApolloClient({
-    uri: "https://general-api-f6ljpbkkwa-uc.a.run.app/auth",
-    cache: new InMemoryCache()
-});
-
 export const client = new ApolloClient({
     uri: "https://general-api-f6ljpbkkwa-uc.a.run.app/",
     cache: new InMemoryCache()
 });
+
+
 
 
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./Views/App/App";
+import App from "./views/App/App";
 import { client } from "./graphql/server";
 import { ApolloProvider } from "@apollo/client";
 import { Provider } from "react-redux";
