13b04db917f4e085300a68a004072ede45cafe38
ndcontrerasr <ndcontrerasr>
2022-01-16 14:10:45 -0500

    Diseños de las funciones de registro y menu desplegable

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "@/Views/App/App";
+import Menu from "@/Views/Menu/Menu";
 import { client } from "./graphql/server";
 import { ApolloProvider } from "@apollo/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
 	<React.StrictMode>
         <ApolloProvider client={client}>
                 <Provider store={store}>
+		        <Menu />
 		        <App />
                 </Provider>
         </ApolloProvider>
